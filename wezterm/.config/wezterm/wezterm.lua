39c84d07536371cf25a7ae3ddd83a861d7d1b5fd
Fabian Gruber <fgruber@Fabians-MacBook-Pro.local>
2024-02-02 10:31:44 +0100

    Adjust wezterm opacity

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,21 +2,21 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- [[ appearance ]]
-config.color_scheme = 'Tokyo Night Storm'
-config.color_scheme_dirs = { '~/.config/wezterm/colorschemes' }
-config.window_background_opacity = 0.99
+config.color_scheme = "Tokyo Night Storm"
+config.color_scheme_dirs = { "~/.config/wezterm/colorschemes" }
+config.window_background_opacity = 0.95
 config.dpi = 144.0
 config.enable_tab_bar = false
 config.font_size = 8.5
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 return config
