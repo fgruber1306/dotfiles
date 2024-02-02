92a14a0ec3ceb0389c0e16e2f1a46545849a9273
Fabian Gruber <fgruber@Fabians-MacBook-Pro.local>
2024-02-02 10:12:47 +0100

    Reset fish config file for M3 Mac migration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,4 @@
-### PATH ###
-set default_path /usr/bin /usr/sbin /bin /sbin
-set -gx PATH ~/Library/Python/3.11/bin $PATH
-set homebrew /usr/local/bin /usr/local/sbin
-set -gx XDG_CONFIG_HOME ~/.config
-set -gx PATH $homebrew $default_path
-set -gx NVM_DIR ~/.nvm
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 ### Commands to run in interactive sessions can go here
 if status is-interactive
