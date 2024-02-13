9c1c88d73052bf8a783b9c5ed7a99fb243ecadd2
Fabian Gruber <f.gruber@limesoda.com>
2024-02-13 20:33:30 +0100

    Change from intelephense to phpactor

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
+        "phpactor",
         "stylua",
         "luacheck",
         "shellcheck",
@@ -12,7 +13,6 @@ return {
         "tailwindcss-language-server",
         "typescript-language-server",
         "css-lsp",
-        "intelephense",
       })
     end,
   },
