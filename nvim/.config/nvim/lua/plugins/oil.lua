e6992427a4bf31544da6e52c2da8b47efa0dd943
Fabian Gruber <f.gruber@limesoda.com>
2024-02-13 20:33:43 +0100

    Make oil show hidden files by default

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,9 +5,16 @@ return {
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
+    default_file_explorer = false,
     float = {
       max_width = 120,
       max_height = 25,
     },
+    view_options = {
+      show_hidden = true,
+    },
+    is_hidden_file = function(name, bufnr)
+      return vim.startswith(name, ".")
+    end,
   },
 }
