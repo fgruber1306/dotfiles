2bb7ef9cc7f0eed38afd91fa3f75d5f4467da9e6
Fabian Gruber <f.gruber@limesoda.com>
2024-02-23 13:11:46 +0100

    Move lualine out of ui and delete incline

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,45 @@
+-- lualine.lua
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "f-person/git-blame.nvim",
+    },
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local gitblame = require("gitblame")
+
+      opts.options = {
+        icons_enabled = true,
+        theme = "catppuccin",
+        component_separators = { left = " ", right = " " },
+        section_separators = { left = "", right = "" },
+        always_divide_middle = true,
+      }
+
+      opts.tabline = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {
+          {
+            gitblame.get_current_blame_text,
+            cond = gitblame.is_blame_text_available,
+          },
+        },
+      }
+
+      opts.sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { "filename", require("utils.lualine").maximize_status },
+        lualine_x = { require("utils.lualine").get_lsps, "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      }
+
+      return opts
+    end,
+  },
+}
