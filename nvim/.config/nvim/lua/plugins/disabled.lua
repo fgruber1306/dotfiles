f3912930be45ddab6c66b6d7e5fcc2ded8c62aa4
Fabian Gruber <f.gruber@limesoda.com>
2024-03-22 10:21:24 +0100

    Enable neotree for magento

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -2,7 +2,6 @@
 
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
   { "echasnovski/mini.bufremove", enabled = false },
