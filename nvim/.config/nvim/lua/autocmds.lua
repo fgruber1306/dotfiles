c2c0c69d927973f06739538f84b0dc6a8b8d9d38
Fabian Gruber <f.gruber@limesoda.com>
2024-06-05 20:59:34 +0200

    Format files

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -26,19 +26,20 @@ vim.api.nvim_create_autocmd('BufReadPre', {
   pattern = { '*' },
   group = vim.api.nvim_create_augroup('bigfile', {}),
   callback = function(args)
-    local match = require('utils').is_large_file(args.buf)
+  local match = require('utils').is_large_file(args.buf)
 
-    if match then
-      vim.opt_local.foldmethod = 'manual'
-      vim.opt_local.swapfile = false
-      vim.opt_local.undolevels = -1
-      vim.opt_local.undoreload = 0
+  if match then
+    vim.opt_local.foldmethod = 'manual'
+    vim.opt_local.swapfile = false
+    vim.opt_local.undolevels = -1
+    vim.opt_local.undoreload = 0
 
-      vim.cmd 'filetype off'
-      vim.cmd 'syntax off'
-    else
-      vim.cmd 'filetype on'
-      vim.cmd 'syntax on'
-    end
+    vim.cmd 'filetype off'
+    vim.cmd 'syntax off'
+  else
+    vim.cmd 'filetype on'
+    vim.cmd 'syntax on'
+  end
   end,
 })
+
--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,31 +1,32 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    local conform = require("conform")
+    local conform = require 'conform'
 
-    conform.setup({
+    conform.setup {
       formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        lua = { "stylua" },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
+        lua = { 'stylua' },
+        php = { 'easy-coding-standard' },
       },
-    })
+    }
 
-    vim.keymap.set({ "n", "v", "x" }, "<leader>mp", function()
-      conform.format({
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>bf', function()
+      conform.format {
         lsp_fallback = true,
         async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
+        timeout_ms = 1001,
+      }
+    end, { desc = 'Format file or range (in visual mode)' })
   end,
 }
 
