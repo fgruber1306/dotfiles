246cddcbe6e9864ee9e61bed5dd0a3af961ca438
Fabian Gruber <f.gruber@limesoda.com>
2024-06-05 20:58:28 +0200

    Make autopairs actually work

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,18 +1,16 @@
 return {
-  "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
+  'windwp/nvim-autopairs',
+  event = { 'InsertEnter' },
   dependencies = {
-    "hrsh7th/nvim-cmp",
+    'hrsh7th/nvim-cmp',
   },
   config = function()
-    -- import nvim-autopairs completion functionality
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-    -- import nvim-cmp plugin (completions plugin)
-    local cmp = require("cmp")
-
-    -- make autopairs and completion work together
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    require('nvim-autopairs').setup {
+      check_ts = true, -- enable treesitter
+    }
+    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+    local cmp = require 'cmp'
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
   end,
 }
 
