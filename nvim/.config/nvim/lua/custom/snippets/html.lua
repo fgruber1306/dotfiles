08e777dd355770d6629215256747840fb2d33554
Fabian Gruber <f.gruber@limesoda.com>
2024-06-24 11:59:35 +0200

    Adjust fformat snippet to already provice the curly brackets

--- nvim/.config/nvim/lua/custom/snippets/html.lua
+++ nvim/.config/nvim/lua/custom/snippets/html.lua
@@ -11,7 +11,7 @@ ls.add_snippets('html', {
   s('fd', fmt('<f:debug maxDepth="20">{{{}}}</f:debug>{}', { i(1), i(0) })),
   s('fda', fmt('<f:debug maxDepth="20">{{_all}}</f:debug>{}', { i(0) })),
   s('fimage', fmt('<f:image src="{}" alt="{}" {}/>{}', { i(1), i(2), i(3), i(0) })),
-  s('fformat', fmt('<f:format.html>{}</f:format.html>{}', { i(1), i(0) })),
+  s('fformat', fmt('<f:format.html>{{{}}}</f:format.html>{}', { i(1), i(0) })),
   s('flink', fmt('<f:link pageUid="{{{}}}" class="{}">\n\t{}\n</f:link>{}', { i(1), i(2), i(3), i(0) })),
   s('ftrans', fmt('<f:translate id="{}" extensionName="{}" />{}', { i(1), i(2), i(0) })),
   s('frender', fmt('<f:render partial="{}" arguments="{{{}}}" />{}', { i(1), i(2), i(0) })),
