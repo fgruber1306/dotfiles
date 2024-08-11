a88f96f5321e9e92b78f398186af722ab6b54018
Fabian Gruber <f.gruber@limesoda.com>
2024-08-11 14:58:19 +0200

    Add Fallback if no range is supplied

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -29,15 +29,21 @@ return {
 		local range_formatting = function()
 				local start_row, _ = unpack(vim.api.nvim_buf_get_mark(0, "<"))
 				local end_row, _ = unpack(vim.api.nvim_buf_get_mark(0, ">"))
-				vim.lsp.buf.format({
-						range = {
-								["start"] = { start_row, 0 },
-								["end"] = { end_row, 0 },
-						},
-						async = true,
-				})
+
+				-- If no range is selected, format the entire buffer
+				if start_row == end_row then
+						vim.lsp.buf.format()
+				else
+						vim.lsp.buf.format({
+								range = {
+										["start"] = { start_row, 0 },
+										["end"] = { end_row, 0 },
+								},
+								async = true,
+						})
+				end
 		end
 
-		vim.keymap.set("v", "<leader>bf", range_formatting, { desc = "Range Formatting" })
+		vim.keymap.set({ 'n', 'v', 'x' }, "<leader>F", range_formatting, { desc = "Range Formatting" })
   end,
 }
