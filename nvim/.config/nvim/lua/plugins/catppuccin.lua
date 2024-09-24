89fbafeffc66f1186daa7599e7e977cd13cd7fec
Fabian Gruber <f.gruber@limesoda.com>
2024-09-24 22:52:30 +0200

    Redo config going with a more lazyvim-like approach

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+      vim.cmd("colorscheme catppuccin-mocha")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
